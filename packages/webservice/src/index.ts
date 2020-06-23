5e02842e8caefbf2fed46ef13ca0394e47b98a3b
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2020-06-24 01:42:26 +0530

    Tra 18 (#19)
    Ajit | Added workflow for Deployment

--- packages/webservice/src/index.ts
+++ packages/webservice/src/index.ts
@@ -1,3 +1,5 @@
 import server from './server';
 
-server.listen('3000', () => { console.log('server started on port: 3000'); });
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => { console.log('server started on port: 3000'); });
