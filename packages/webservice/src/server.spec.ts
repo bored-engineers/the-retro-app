686f9946bf67b415358be96928054a69bc7e1ce9
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2020-06-23 22:09:24 +0530

    Ajit | fixed test

--- packages/webservice/src/server.spec.ts
+++ packages/webservice/src/server.spec.ts
@@ -3,7 +3,11 @@ import express from 'express';
 
 import supertest from 'supertest';
 
-jest.mock('express', () => jest.fn(() => ({ use: jest.fn(), Router: () => jest.fn() })));
+jest.mock('express', () => {
+  const mockedExpress = jest.fn(() => ({ use: jest.fn(), Router: () => jest.fn() }));
+  Object.defineProperty(mockedExpress, "static", { value: jest.fn() });
+  return mockedExpress;
+});
 jest.mock('./routes', () => ({ mainRouter: jest.fn() }));
 jest.mock('./db');
 describe('Server', () => {
