507361f06ceb4e7f48b2c35deb6e9490651a06e2
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2020-06-23 21:41:18 +0530

    Ajit | Added deploy app workflow

--- packages/webservice/src/db.ts
+++ packages/webservice/src/db.ts
@@ -1,7 +1,8 @@
-import MongoClient, {Db} from 'mongodb'
+import MongoClient, { Db } from 'mongodb'
 import config from 'config';
 
-let db:Db = null;
+let db: Db = null;
+const DB_URL = process.env.DB_URL || config.get('db.url');
 
 var DbConnection = async function () {
     try {
@@ -9,8 +10,8 @@ var DbConnection = async function () {
             console.log(`db connection is already alive`);
             return db;
         } else {
-            console.log(`getting new db connection`);
-            db = await (await MongoClient.connect(config.get('db.url'))).db();
+            console.log(`getting new db connection to ${DB_URL}`);
+            db = await (await MongoClient.connect(DB_URL)).db();
             return db;
         }
     } catch (e) {
--- packages/webservice/src/server.ts
+++ packages/webservice/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import path from 'path';
 import MongoDB from './db';
 import mainRouter from './routes';
 
@@ -8,7 +8,7 @@ const server = express();
 MongoDB();
 
 server.use(bodyParser.json());
-
+server.use(express.static(path.join(__dirname, 'public')));
 server.use('/api', mainRouter);
 
 export default server;
