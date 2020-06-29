ac187e984423cf8706aa2f8499065ed4a2470e84
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2020-06-29 22:23:27 +0530

    Ajit| Added error handling for non existent board.

--- packages/webservice/src/routes/board-route.ts
+++ packages/webservice/src/routes/board-route.ts
@@ -1,15 +1,23 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { BoardService } from '../services';
 
 const boardRouter = Router();
 const boardService = new BoardService();
 
-boardRouter.get('/:boardId', async (req: Request, res: Response) => {
-    res.send(await boardService.getBoard(req.params.boardId));
+boardRouter.get('/:boardId', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.send(await boardService.getBoard(req.params.boardId));
+    } catch (e) {
+        next(e);
+    }
 });
 
-boardRouter.post('/', async (_req: Request, res: Response) => {
-    res.send(await boardService.createBoard());
+boardRouter.post('/', async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.send(await boardService.createBoard());
+    } catch (e) {
+        next(e);
+    }
 });
 
 export default boardRouter;
--- packages/webservice/src/server.ts
+++ packages/webservice/src/server.ts
@@ -1,14 +1,19 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import MongoDB from './db';
 import mainRouter from './routes';
+import errorHandlerMiddleware from './errors/error-handler-middleware';
 
 const server = express();
+
 MongoDB();
 
 server.use(bodyParser.json());
 server.use(express.static(path.join(__dirname, 'public')));
 server.use('/api', mainRouter);
+
+server.use(errorHandlerMiddleware);
+
 
 export default server;
