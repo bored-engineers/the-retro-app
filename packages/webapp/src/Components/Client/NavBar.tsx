825c975bd6563182d338ccef19e5473b7cee47da
saptarshi.deb <sap7deb@gmail.com>
2020-07-02 04:02:01 +0530

    Saptarshi | Added cards and onclick functions for board

--- packages/webapp/src/Components/Client/NavBar.tsx
+++ packages/webapp/src/Components/Client/NavBar.tsx
@@ -6,7 +6,6 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,12 +32,12 @@ export default () => {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            <Link to='/'>
-
+            <Button href='/' className={classes.title}>
+            <Typography variant="h6" color="primary" className={classes.title}>
               The Retro App
-            </Link>
-          </Typography>
+              </Typography>
+            </Button>
+         
           <Button color="inherit">Change BG</Button>
         </Toolbar>
       </AppBar>
