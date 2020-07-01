478643698cac446480a3565dbea60638b34ece95
Amitesh Arya <amiteshtoharya4@gmail.com>
2020-07-01 21:43:25 +0530

    Amitesh | Add url not found component

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import LandingPage from './Components/Client/LandingPage';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Board from './Components/Client/Board';
+import NotFound from './Components/Client/NotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route path="/" exact children={<LandingPage />} />
           <Route path="/board/:id" children={<Board />} />
+          <Route path="*" children={<NotFound />} />
         </Switch>
       </BrowserRouter>
   );
