fd3cb56102bb0fd6f864ca8d4ee4f490f5a573d6
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2020-07-04 23:52:15 +0530

    Ajit | Added Create Board Feature
    - Api Call
    - Error Handling
    - Success Handling

--- packages/webapp-v2/src/components/landing-page/LandingPage.tsx
+++ packages/webapp-v2/src/components/landing-page/LandingPage.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import FormControl from '@material-ui/core/FormControl'
 import InputLabel from '@material-ui/core/InputLabel'
 import Input from '@material-ui/core/Input'
-import boardImage from '../../board.svg';
+import LinearProgress from '@material-ui/core/LinearProgress'
+import FormHelperText from '@material-ui/core/FormHelperText'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import {createBoard} from '../../services/board.service';
-
+import { createBoard } from '../../services/board.service';
 import './LandingPage.scss';
+import boardImage from '../../board.svg';
 
 
 const LandingPage = () => {
-    createBoard();
+
+    const [boardId, setBoardId] = useState('');
+    const [createBoardProgress, setCreateBoardProgress] = useState(false);
+    const [createBoardSuccess, setCreateBoardSuccess] = useState(false);
+    const [createBoardError, setCreateBoardError] = useState(false);
+
+    const createBoardHandler = () => {
+        setCreateBoardProgress(true);
+        setCreateBoardError(false);
+        setCreateBoardSuccess(false);
+        createBoard()
+            .then(boardId => { 
+                setBoardId(boardId); 
+                setCreateBoardSuccess(true);
+            })
+            .catch(error => {
+                setCreateBoardError(true);
+            }).finally(() => {
+                setCreateBoardProgress(false)
+            });
+    }
+
     return (
         <React.Fragment>
-            
             <Grid container direction="column" justify="center" alignItems="center" className="landingPage">
                 <Card className='form'>
                     <CardContent>
@@ -32,19 +53,22 @@ const LandingPage = () => {
                     </CardContent>
                     <div className="image"><img src={boardImage} alt="" /></div>
                     <div className="create-board">
-                        <Button variant="contained" color="primary">Create a New Board</Button>
+                        <Button variant="contained" color="primary" onClick={createBoardHandler} disabled={createBoardProgress} >Create a New Board</Button>
+                        {createBoardProgress && <LinearProgress />}
+                        {createBoardError && <FormHelperText error>There is some problem creating board. Try Again Later...</FormHelperText>}
+                        {createBoardSuccess && <FormHelperText>Your board created successfully. You can join now...</FormHelperText>}
                     </div>
                     <Divider variant="middle" flexItem />
                     <div className="join-board">
-                        <Typography className='join-board-title' color="textSecondary" gutterBottom > 
-                            Join an existing board 
+                        <Typography className='join-board-title' color="textSecondary" gutterBottom >
+                            Join an existing board
                         </Typography>
                         <FormControl fullWidth>
-                            <InputLabel htmlFor="boardId" variant="standard"  margin="dense">Board ID</InputLabel>
-                            <Input id="boardId" fullWidth />
+                            <InputLabel htmlFor="boardId" variant="standard" margin="dense">Board ID</InputLabel>
+                            <Input id="boardId" fullWidth value={boardId} />
                         </FormControl>
                         <FormControl fullWidth>
-                            <InputLabel htmlFor="username" variant="standard"  margin="dense">Your Name</InputLabel>
+                            <InputLabel htmlFor="username" variant="standard" margin="dense">Your Name</InputLabel>
                             <Input id="username" fullWidth />
                         </FormControl>
                         <Button variant="contained" color="primary">Join Now</Button>
--- packages/webapp-v2/src/services/board.service.ts
+++ packages/webapp-v2/src/services/board.service.ts
@@ -4,15 +4,14 @@ const SERVICE_URL = process.env.REACT_APP_SERVICE_URL || '';
 
 
 const getBoard = async (boardId: string) => {
-    
+
 }
 
 const createBoard = async () => {
     console.log(`Connecting to backend: ${SERVICE_URL}`);
-    const result = await axios.post(`${SERVICE_URL}/api/boards`);
-    console.log('result', result);
+    const { boardId } = (await axios.post(`${SERVICE_URL}/api/boards`)).data;
+    return boardId;
 }
-
 
 export {
     getBoard,
