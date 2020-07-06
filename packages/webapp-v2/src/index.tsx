32a8164c526653fbe4c67e25ef94c26a2dd440cb
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2020-07-06 18:14:41 +0530

    Ajit | Major refactoring and styling

--- packages/webapp-v2/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
-import './index.scss';
-
-import App from './App';
-
-ReactDOM.render(<React.StrictMode> <CssBaseline /><App /></React.StrictMode>, document.getElementById('root'));
