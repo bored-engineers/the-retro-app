ad09974aacea735a3cc14533f03a4c6a5ad75681
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2020-07-29 22:20:29 +0530

    Ajit | Added Hash router

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 import {ErrorBoundary} from './components/error-boundary/ErrorBoundary';
 import LandingPage from './components/landing-page/LandingPage';
 import Boards from './components/boards/Boards';
 
 const App = () => (
     <ErrorBoundary>
-        <Router>
+        <HashRouter>
             <Route path='/' exact component={LandingPage} />
             <Route path='/boards' component={Boards} />
-        </Router>
+        </HashRouter>
     </ErrorBoundary>
 );
 
