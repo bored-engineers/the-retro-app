422748b2a511f5c5b2c24b28454d998da5b97252
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2020-09-17 00:21:17 +0530

    Ajit | Fixed typo

--- packages/webapp/src/components/common/footer/Footer.tsx
+++ packages/webapp/src/components/common/footer/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
             <AppBar position="fixed" color="default" className='custom-footer'>
                 <Toolbar className="toolbar">
                     <Typography className='branding-text' variant="caption" display="block" gutterBottom>From the minds of <a rel='noopener noreferrer' target='_blank' href='https://github.com/orgs/bored-engineers/people'>Bored Engineers</a></Typography>
-                    <Typography className='feedback-text' variant="caption" display="block" gutterBottom>Please provide your valueble <a href='#/survey'>feedback here</a>.</Typography>
+                    <Typography className='feedback-text' variant="caption" display="block" gutterBottom>Please provide your valuable <a href='#/survey'>feedback here</a>.</Typography>
                 </Toolbar>
             </AppBar>
         </React.Fragment>
